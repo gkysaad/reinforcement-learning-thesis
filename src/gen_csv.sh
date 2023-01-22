f452cb5cd2ce9fd8c8a3ec0e78af048d084cc91d
gkysaad <g.saad@mail.utoronto.ca>
2023-01-22 17:01:25 -0500

    clean up

--- src/gen_csv.sh
+++ src/gen_csv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Generates csv files from the tensorboard logs
+
 for ((i=0;i<=9;i++)); 
 do 
    python tensorboard_to_csv.py --dir "C:\Users\George\OneDrive\Documents\University\Y4F\Thesis\thesis-rl-project-main\thesis-rl-project-main\src\results\drl\CartPole-v0\final_n_env_10_arch_064_064_lr_2.50e-3_seed_0${i}00"
